ff67697ae1e6ab2b1db17f25578e1d70588383d2
ygd <958494352@qq.com>
2018-09-29 15:26:21 +0800

    删除jar包runtime属性

--- src/main/java/com/blog/controller/UserController.java
+++ src/main/java/com/blog/controller/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <p>
  *  前端控制器
@@ -23,6 +26,15 @@ public class UserController {
     @GetMapping("/index")
     public String index() {
         log.info("-------------------------------->这是首页");
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list2.add(2);
+        list2.add(3);
+        list1.retainAll(list2);
+        list1.forEach(System.out::println);
+
         return "index";
     }
 
