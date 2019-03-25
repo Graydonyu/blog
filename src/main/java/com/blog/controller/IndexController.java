405a853c6611d3d3d89c8e1b1d6261cd30559e5d
yuguidong <958494352@qq.com>
2019-03-25 23:18:16 +0800

    配置验证码生成工具

--- src/main/java/com/blog/controller/IndexController.java
+++ src/main/java/com/blog/controller/IndexController.java
@@ -3,18 +3,19 @@ package com.blog.controller;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.blog.entity.Post;
-import com.blog.service.PostService;
+import com.google.code.kaptcha.Producer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author ygd
@@ -25,9 +26,11 @@ import java.util.Map;
 @RequestMapping("/index")
 public class IndexController extends BaseController {
 
+    @Autowired
+    Producer producer;
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         Page<Post> page = new Page<>();
         page.setCurrent(1);
         page.setSize(10);
@@ -44,5 +47,14 @@ public class IndexController extends BaseController {
         log.info("-------------------------------" + page.getPages());
 
         return "index";
+    }
+
+    @GetMapping("/captcha")
+    public void captcha(HttpServletResponse response){
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setContentType("image/jpeg");
+
+        //生成文字验证码
+        producer.createText();
     }
 }
