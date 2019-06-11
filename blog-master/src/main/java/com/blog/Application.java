d7ff1fae0d3f289aeb0de6fad3ed18c4d060b93c
ygd <958494352@qq.com>
2019-06-11 15:20:55 +0800

    网关服务，所有项目注册到eureka

--- blog-master/src/main/java/com/blog/Application.java
+++ blog-master/src/main/java/com/blog/Application.java
@@ -2,10 +2,12 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
